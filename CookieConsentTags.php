59aad486083b2af2c7a1ac8e3c47b0a3f4ab4aaa
Alexander Bangert <alex@alexbangert.de>
2018-12-20 16:13:42 +0100

    Addon setup

---
+++ CookieConsentTags.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Statamic\Addons\CookieConsent;
+
+use Statamic\Extend\Tags;
+
+class CookieConsentTags extends Tags
+{
+    /**
+     * The {{ cookie_consent }} tag
+     *
+     * @return string|array
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * The {{ cookie_consent:example }} tag
+     *
+     * @return string|array
+     */
+    public function example()
+    {
+        //
+    }
+}
