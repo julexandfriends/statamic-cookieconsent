d27cfd387e5b28728d2dfa7f5eaeb653c84f6ecc
Alexander Bangert <alex@alexbangert.de>
2018-12-20 16:41:37 +0100

    First Version

--- CookieConsentTags.php
+++ CookieConsentTags.php
@@ -2,6 +2,8 @@
 
 namespace Statamic\Addons\CookieConsent;
 
+use Statamic\API\File;
+use Statamic\API\Parse;
 use Statamic\Extend\Tags;
 
 class CookieConsentTags extends Tags
@@ -13,16 +15,22 @@ class CookieConsentTags extends Tags
      */
     public function index()
     {
-        //
-    }
+        $data = [
+            "theme" => $this->getConfig("theme", "edgeless"),
+            "message" => $this->getConfig("message"),
+            "dismiss" => $this->getConfig("dismiss"),
+            "link_href" => $this->getConfig("link_href"),
+            "link_caption" => $this->getConfig("link_caption"),
+            "position" => $this->getConfig("position"),
+            "popup_background_color" => $this->getConfig("popup_background_color"),
+            "popup_text_color" => $this->getConfig("popup_text_color"),
+            "button_background_color" => $this->getConfig("button_background_color"),
+            "button_text_color" => $this->getConfig("button_text_color"),
+        ];
 
-    /**
-     * The {{ cookie_consent:example }} tag
-     *
-     * @return string|array
-     */
-    public function example()
-    {
-        //
+        $file_path = "{$this->getDirectory()}/resources/views/index.html";
+        $file_content = File::get($file_path);
+
+        return Parse::template($file_content, $data);
     }
 }
